5b4c3dffa2dad11d9707e5d08cd75a799ee8c31b
dw113073 <dw113073@alibaba-inc.com>
2021-01-15 16:57:36 +0800

    up

---
+++ src/test/java/com/lrn/test/list/Test.java
@@ -0,0 +1,21 @@
+package com.lrn.test.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author dengwei
+ * @since 2020/11/20.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        List<String> a = new ArrayList<>();
+        a.add("test");
+        List<String> b = new ArrayList<>();
+        b.add("test");
+        b.add("test1");
+
+        System.out.println(a.equals(b));
+    }
+}
